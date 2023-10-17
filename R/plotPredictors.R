b4981ffae9e13ae662debe6f1903accc615f17f7
sharnag <110274461+sharnag@users.noreply.github.com>
2023-10-17 23:51:26 +1300

    Added default method for addCols

--- R/plotPredictors.R
+++ R/plotPredictors.R
@@ -1,6 +1,6 @@
 #' Plot Logit of Response against Numerical Predictors
 #'
-#' S3 Method on glm object used to return plots of the logit of the response vs numerical predictors. Used during model diagnostics.
+#' S3 Method for glm object used to return plots of the logit of the response vs numerical predictors. Used during model diagnostics.
 #'
 #' @param fittedModel A fitted `glm` model object of family `binomial`.
 #' @param interactive `logical`. If `TRUE` then return an interactive plot; if `FALSE`, return ggplot objects (default).
@@ -97,33 +97,3 @@ plotPredictors.glm <- function(fittedModel, smooth=T, interactive=F){
 
 }
 
-
-#' Add Columns to glm Data
-#'
-#' Internal Function used copy the model dataframe and add additional columns such as the predicted probability and logit of the response. Used by `plotPredictors` during model diagnostics
-#'
-#' @param fittedModel A fitted `glm` model object of family `binomial`.
-#' @keywords fitted
-#' @examples
-#' new_data <- addCols(fittedModel)
-#'
-#' @importFrom magrittr %>%
-#' @importFrom dplyr mutate n
-#'
-#' @export
-addCols <- function(fittedModel) UseMethod('addCols')
-
-#'
-#' @export
-addCols.glm <-function(fittedModel){
-  data_copy <- fittedModel$data
-
-  # Bind logit(.fitted) etc to the dataframe
-  data_copy <- broom::augment(fittedModel) %>%
-    dplyr::mutate(index = 1:dplyr::n())
-
-  # Add the predicted probabilities
-  data_copy$predprob <- fittedModel$fitted.values
-
-  return(data_copy)
-}
